2ffacff8c52e8363a6cd1a32cc8223eecdc789a5
Charley <charley.geoffroy@etu.umontpellier.fr>
2023-11-02 13:51:40 +0100

    feat(gateway-registry): add gateway and discovery service registry for profile service
    Signed-off-by: Charley <charley.geoffroy@etu.umontpellier.fr>

--- micro-services/profile-service/src/main/java/com/seasonalworkers/profile/ProfileApplication.java
+++ micro-services/profile-service/src/main/java/com/seasonalworkers/profile/ProfileApplication.java
@@ -1,9 +1,11 @@
 package com.seasonalworkers.profile;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProfileApplication {
 
 	public static void main(String[] args) {
